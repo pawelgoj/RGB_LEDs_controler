92189a7b9867003d3c59a2943487a7fe2c6c3841
Pawel <71873238+pawelgoj@users.noreply.github.com>
2023-01-14 17:19:48 +0100

    Remove some bugs

--- visual_effects.py
+++ visual_effects.py
@@ -9,7 +9,6 @@ class VisualEffects:
     interval: int = 1000
     time_interval: int = 1000
     current_effect: str = 'breath_effect'
-    conversion: float = 2 / 65535
     constant: int = 65535
     
     def __init__(self, list_led_b: list[machine.PWM], 
@@ -45,9 +44,9 @@ class VisualEffects:
             change_specific_color(list_led_r, int(temp_colors[2] 
                                                   * (self.time_interval - (t - 3 * self.time_interval)) /self.time_interval))
         else:
-            self.time = utime.ticks_ms()
+            self.time = None
             
-    def color_change(self, list_led_b: list, list_led_g: list, list_led_r: list, temp_colors: list) -> None:
+    def color_change(self, list_led_b: list, list_led_g: list, list_led_r: list) -> None:
         if self.time == None:
             self.time = utime.ticks_ms()
         elif utime.ticks_diff(utime.ticks_ms(), self.time) < self.time_interval:
@@ -73,14 +72,12 @@ class VisualEffects:
             change_specific_color(list_led_g, 0)
             change_specific_color(list_led_r, 0)
         elif  (t := utime.ticks_diff(utime.ticks_ms(), self.time)) < 6 * self.time_interval:
-            change_specific_color(list_led_b, int(self.constant *  (self.time_interval - (t - 5 * self.time_interval)) /self.time_interval))
+            change_specific_color(list_led_b, int(self.constant 
+                                                  * (self.time_interval - (t - 5 * self.time_interval)) /self.time_interval))
             change_specific_color(list_led_g, 0)
             change_specific_color(list_led_r, int(self.constant * ((t - 5 * self.time_interval) /self.time_interval)))
         else:
-            self.time = utime.ticks_ms()
-            change_specific_color(list_led_b, 0)
-            change_specific_color(list_led_g, 0)
-            change_specific_color(list_led_r, self.constant)
+            self.time = None
     
     def effect_loop(self, potentiometer: machine.ADC, state_of_button_change_color: ButtonChangeColor, list_led_b: list, list_led_g: list, list_led_r: list, temp_colors: list) -> None: 
         
@@ -90,14 +87,15 @@ class VisualEffects:
             if state_of_button_change_color.state in ('save', 'loaded'):
                 break
             elif state_of_button_change_color.state is 'change':
+                self.time = None
                 state_of_button_change_color.state = 'visual_effects'
                 self.current_effect = 'breath_effect' if self.current_effect is 'color_change' else 'color_change'
                 
             if self.current_effect == 'breath_effect': 
-                self.time_interval = int(potentiometer.read_u16() * self.conversion * self.interval) + 100
+                self.time_interval = int((((potentiometer.read_u16() / self.constant) * self.interval) /2)) + 100
                 self.breath_effect(list_led_b, list_led_g, list_led_r, temp_colors)
             elif self.current_effect == 'color_change':
-                self.time_interval = int(potentiometer.read_u16() * self.conversion * self.interval) + 100
-                self.color_change(list_led_b, list_led_g, list_led_r, temp_colors)
+                self.time_interval = int((((potentiometer.read_u16() / self.constant) * self.interval) /2)) + 100
+                self.color_change(list_led_b, list_led_g, list_led_r)
             else: 
                 print("No implement effect!!!!!")
