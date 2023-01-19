48b0e7e25ba265359acb051a70f5d630e7a133b0
Pawel <71873238+pawelgoj@users.noreply.github.com>
2023-01-19 18:40:28 +0100

    Refactoring

--- functions.py
+++ functions.py
@@ -1,7 +1,129 @@
 import machine
+import utime
+import os
+from button import ButtonChangeColor
+from button import Button
 
 
 def change_specific_color(list_of_pins: list[machine.PWM], result: int) -> None:
     for item in list_of_pins:
         item.duty_u16(result)
         
+        
+def prepare_pins(red_led_pins: list[int], green_led_pins: list[int], 
+                 blue_led_pins: list[int]) -> tuple[list[machine.PWM], 
+                                                    list[machine.PWM], 
+                                                    list[machine.PWM]]:
+    """Function return the tuple of lists of machine.PWM for pins for leds
+
+    Args:
+        red_led_pins (list[int]): list of pins connected to red leds 
+        green_led_pins (list[int]): list of pins connected to green leds
+        blue_led_pins (list[int]): list of pins connected to blue leds
+
+    Raises:
+        Exception: If length of list of pins connected to leds are not the same.
+
+    Returns:
+        tuple[list[machine.PWM], list[machine.PWM], list[machine.PWM]]: _description_
+    """
+    
+    if len(blue_led_pins) == len(green_led_pins) == len(red_led_pins):
+        list_led_b = [machine.PWM(machine.Pin(item))
+                    for item in blue_led_pins]
+        
+        list_led_g = [machine.PWM(machine.Pin(item))
+                    for item in green_led_pins]
+        
+        list_led_r = [machine.PWM(machine.Pin(item))
+                for item in red_led_pins]
+        
+        for i in range(len(list_led_r)):
+            list_led_g[i].freq(1000)
+            list_led_b[i].freq(1000)
+            list_led_r[i].freq(1000)
+    else: 
+        raise Exception("Length of red_led_pins, blue_led_pins and" +
+                        "green_led_pins must be the same!")
+        
+    return list_led_b, list_led_g, list_led_r
+        
+        
+def load_saved_colors_form_file() -> tuple[dict, bool]:
+    """Loads saved colors from file data.txt, if file does not exist returns: 
+        colors = {'b': 0, 'g': 0, 'r': 0} and first_run = True. 
+
+    Returns:
+        tuple[dict, bool]: ({'b': 0, 'g': 0, 'r': 0}, first_run)
+    """
+    
+    colors = {'b': 0, 'g': 0, 'r': 0}
+    first_run = True
+    
+    for item in os.listdir():
+        if 'data.txt' == f'{item}':
+            with open('data.txt', 'r') as file:
+                string_data = file.read()
+                splitted = string_data.split(' ')
+                
+                colors['b'] = int(splitted[0])
+                colors['g'] = int(splitted[1])
+                colors['r'] = int(splitted[2])
+                
+                del string_data
+                first_run = False
+    
+    return colors, first_run
+        
+        
+def button_change_color_fn(state_of_button_change_color: ButtonChangeColor, 
+                        time: int|None, after_saved: bool, button_change_color: Button, last_button_state: str) \
+                        -> tuple[ButtonChangeColor, int|None, bool, str]:
+    """Function specifying the state of the color change button
+
+    Args:
+        state_of_button_change_color (str): colors 'r', 'g', 'b' or 'save', 'loaded'
+        time (int): time.ticks_ms()
+        after_saved (bool): Was the previous state_of_button_change_color 'save'? True/False
+        button_change_color (Button): Button object
+        last_button_state: (str): 
+    Returns:
+        tuple(state_of_button_change_color, iteration, after_saved, last_button_state)
+    """
+    pressed = button_change_color.pressed
+    
+    if (pressed is True) and (time is None):
+        time = utime.ticks_ms()
+        time_diff = -1
+    elif (pressed is False) and (time is not None):
+        time_diff = utime.ticks_diff(utime.ticks_ms(), time)
+        time = None
+    else:
+        time_diff = -1
+    
+    if after_saved:
+        after_saved = False
+        state_of_button_change_color.state = 'loaded'
+        
+    elif time_diff < 1500 and time_diff >= 0: 
+
+        if state_of_button_change_color.state == 'b':
+            state_of_button_change_color.state = 'g'
+        elif state_of_button_change_color.state == 'g':
+            state_of_button_change_color.state = 'r'
+        elif state_of_button_change_color.state == 'r':
+            state_of_button_change_color.state = 'b'
+        elif state_of_button_change_color.state == 'loaded':
+            state_of_button_change_color.state = 'b'
+        elif state_of_button_change_color.state == 'visual_effects':
+            state_of_button_change_color.state = 'change'
+            
+    elif (time_diff < 4000) and (time_diff >= 1500):
+        last_button_state = state_of_button_change_color.state
+        state_of_button_change_color.state = 'save'
+        after_saved = True
+        
+    elif time_diff >= 4000:
+        state_of_button_change_color.state = 'visual_effects'
+        
+    return state_of_button_change_color, time, after_saved, last_button_state
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import machine
 import _thread
-import os
-import utime
 from visual_effects import VisualEffects
 from button import Button
 from functions import change_specific_color
+from functions import prepare_pins
+from functions import button_change_color_fn
+from functions import load_saved_colors_form_file
 from button import ButtonChangeColor
 
 
@@ -13,97 +14,13 @@ button_change_color_pin: machine.Pin = machine.Pin(16, machine.Pin.IN,
 
 state_of_button_change_color: ButtonChangeColor = ButtonChangeColor('b')
 
-
 button_on_off_pressed: bool = False
 
 start_button_oscillation_time: int|None = 0
 
 
-        
-def load_saved_colors_form_file() -> tuple[dict, bool]:
-    """Loads saved colors from file data.txt, if file does not exist returns: 
-        colors = {'b': 0, 'g': 0, 'r': 0} and first_run = True. 
-
-    Returns:
-        tuple[dict, bool]: ({'b': 0, 'g': 0, 'r': 0}, first_run)
-    """
-    
-    colors = {'b': 0, 'g': 0, 'r': 0}
-    first_run = True
-    
-    for item in os.listdir():
-        if 'data.txt' == f'{item}':
-            with open('data.txt', 'r') as file:
-                string_data = file.read()
-                splitted = string_data.split(' ')
-                
-                colors['b'] = int(splitted[0])
-                colors['g'] = int(splitted[1])
-                colors['r'] = int(splitted[2])
-                
-                del string_data
-                first_run = False
-    
-    return colors, first_run
-        
-        
-
-
-def button_change_color_fn(state_of_button_change_color: ButtonChangeColor, 
-                        time: int|None, after_saved: bool, button_change_color: Button, last_button_state: str) \
-                        -> tuple[ButtonChangeColor, int|None, bool, str]:
-    """Function specifying the state of the color change button
-
-    Args:
-        state_of_button_change_color (str): colors 'r', 'g', 'b' or 'save', 'loaded'
-        time (int): time.ticks_ms()
-        after_saved (bool): Was the previous state_of_button_change_color 'save'? True/False
-        button_change_color (Button): Button object
-        last_button_state: (str): 
-    Returns:
-        tuple(state_of_button_change_color, iteration, after_saved, last_button_state)
-    """
-    pressed = button_change_color.pressed
-    
-    if (pressed is True) and (time is None):
-        time = utime.ticks_ms()
-        time_diff = -1
-    elif (pressed is False) and (time is not None):
-        time_diff = utime.ticks_diff(utime.ticks_ms(), time)
-        time = None
-    else:
-        time_diff = -1
-    
-    if after_saved:
-        after_saved = False
-        state_of_button_change_color.state = 'loaded'
-        
-    elif time_diff < 1500 and time_diff >= 0: 
-
-        if state_of_button_change_color.state == 'b':
-            state_of_button_change_color.state = 'g'
-        elif state_of_button_change_color.state == 'g':
-            state_of_button_change_color.state = 'r'
-        elif state_of_button_change_color.state == 'r':
-            state_of_button_change_color.state = 'b'
-        elif state_of_button_change_color.state == 'loaded':
-            state_of_button_change_color.state = 'b'
-        elif state_of_button_change_color.state == 'visual_effects':
-            state_of_button_change_color.state = 'change'
-            
-    elif (time_diff < 4000) and (time_diff >= 1500):
-        last_button_state = state_of_button_change_color.state
-        state_of_button_change_color.state = 'save'
-        after_saved = True
-        
-    elif time_diff >= 4000:
-        state_of_button_change_color.state = 'visual_effects'
-        
-    return state_of_button_change_color, time, after_saved, last_button_state
-
 def button_on_off_thread() -> None:
     
-    import utime 
     """The thread for handling button events
     """
     
@@ -121,46 +38,6 @@ def button_on_off_thread() -> None:
                                    time, after_saved, button_change_color, last_button_state)
             
 
-def prepare_pins(red_led_pins: list[int], green_led_pins: list[int], 
-                 blue_led_pins: list[int]) -> tuple[list[machine.PWM], 
-                                                    list[machine.PWM], 
-                                                    list[machine.PWM]]:
-    """Function return the tuple of lists of machine.PWM for pins for leds
-
-    Args:
-        red_led_pins (list[int]): list of pins connected to red leds 
-        green_led_pins (list[int]): list of pins connected to green leds
-        blue_led_pins (list[int]): list of pins connected to blue leds
-
-    Raises:
-        Exception: If length of list of pins connected to leds are not the same.
-
-    Returns:
-        tuple[list[machine.PWM], list[machine.PWM], list[machine.PWM]]: _description_
-    """
-    
-    if len(blue_led_pins) == len(green_led_pins) == len(red_led_pins):
-        list_led_b = [machine.PWM(machine.Pin(item))
-                    for item in blue_led_pins]
-        
-        list_led_g = [machine.PWM(machine.Pin(item))
-                    for item in green_led_pins]
-        
-        list_led_r = [machine.PWM(machine.Pin(item))
-                for item in red_led_pins]
-        
-        for i in range(len(list_led_r)):
-            list_led_g[i].freq(1000)
-            list_led_b[i].freq(1000)
-            list_led_r[i].freq(1000)
-    else: 
-        raise Exception("Length of red_led_pins, blue_led_pins and" +
-                        "green_led_pins must be the same!")
-        
-    return list_led_b, list_led_g, list_led_r
-
-
-
 potentiometer: machine.ADC = machine.ADC(26)
 list_led_b, list_led_g, list_led_r = prepare_pins([15, 12, 9], [14, 11, 8], [13, 10, 7])
 
@@ -173,6 +50,18 @@ _thread.start_new_thread(button_on_off_thread, ())
 
 temp_colors = [colors['b'], colors['g'], colors['r']]
 visual_effects = VisualEffects(list_led_b, list_led_g, list_led_r, temp_colors)
+
+
+if first_run:
+    change_specific_color(list_led_b, 0)
+    change_specific_color(list_led_g, 0)
+    change_specific_color(list_led_r, 0)
+    state_of_button_change_color.state = 'b'
+else: 
+    change_specific_color(list_led_b, 0)
+    change_specific_color(list_led_g, 0)
+    change_specific_color(list_led_r, 0)
+    state_of_button_change_color.state = 'loaded'
 
 #main loop
 while True:
@@ -210,14 +99,3 @@ while True:
         visual_effects.current_effect = 'breath_effect'
         visual_effects.effect_loop(potentiometer, state_of_button_change_color, list_led_b, list_led_g, 
                                    list_led_r, temp_colors)
-    
-    elif not first_run:
-        change_specific_color(list_led_b, 0)
-        change_specific_color(list_led_g, 0)
-        change_specific_color(list_led_r, 0)
-        state_of_button_change_color.state = 'loaded'
-        
-    elif first_run:
-        change_specific_color(list_led_b, 0)
-        change_specific_color(list_led_g, 0)
-        change_specific_color(list_led_r, 0)
